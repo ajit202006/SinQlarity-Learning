23e799d0c71fec2140e71e543acd5ce75af26e41
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-06-18 19:08:47 +0530

    Created new series

--- Programming/DynamicSeries.py
+++ Programming/DynamicSeries.py
@@ -9,3 +9,19 @@ while i<=end:
     print(start)
     start+=i
     i+=1
+
+# Another Series Printing program
+# here for element at odd position we add 3 and for even position we subtract 1 to get next element
+
+start=1
+gap = 3
+end=int(input("Enter number of terms in the series: "))
+i=1
+while i <=end:
+    print(start)
+    start+=gap
+    if gap==3:
+        gap =-1
+    else:
+        gap=3
+    i+=1
