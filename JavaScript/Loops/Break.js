15d734d542141a1eb33af503cb27267e84865483
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-06-23 19:33:53 +0530

    Created a break statement demo file in js

---
+++ JavaScript/Loops/Break.js
@@ -0,0 +1,11 @@
+// Use of break statement in loop
+// exiting a loop when i exceeds 13
+for(let i=1; i<=20; i++){
+    //Block of code
+    console.log("I am printing",i);
+
+    if (i==13){
+
+        break;//Stops execution when i==13
+    }
+}
