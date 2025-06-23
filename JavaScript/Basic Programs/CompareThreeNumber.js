4e404087ba85bc50eddf47335101f0f02e48845d
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-06-23 18:37:55 +0530

    Created threee numbers comparison program in js

---
+++ JavaScript/Basic Programs/CompareThreeNumber.js
@@ -0,0 +1,17 @@
+var x=56;
+var y=75;
+var z=69;
+
+if (x>y){
+    if (x>z){
+        console.log("X is greater than all.");
+    }else{
+        console.log("Z is greater than all.")
+    }
+}else{
+    if (y>z){
+        console.log("Y is greater than all.")
+    }else{
+        console.log("Z is greater than all.")
+    }
+}
