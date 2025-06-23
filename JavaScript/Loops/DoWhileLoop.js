3d15d89c8eab0542490d348b91f4d0a7bd722a86
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-06-23 19:27:00 +0530

    Created do-while loop basic program in js

---
+++ JavaScript/Loops/DoWhileLoop.js
@@ -0,0 +1,11 @@
+// Basic do...while loop structure
+
+let i=1; // Initialization
+
+do{
+    //Block of loop
+    console.log("I am do-while loop, I am printing ",i*4);
+
+    i++;// Increment inside the block of loop
+
+}while(i<=10); // Condition checking at the exit point of the loop
--- JavaScript/Loops/WhileLoop.js
+++ JavaScript/Loops/WhileLoop.js
@@ -2,10 +2,10 @@
 
 let i=1; // Initialization  
 
-while (i<=10 )//Condition
+while (i<=10 )//Condition checking at the beginning of the loop
 {
     //Block of Code 
-    console.log("I am while loop, I am printing ",i*2) 
-    
+    console.log("I am while loop, I am printing ",i*2);
+
     i++;// Increment inside the block of code
 }
