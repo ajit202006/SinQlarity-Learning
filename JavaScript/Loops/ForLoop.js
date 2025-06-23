317631264a61d491825c675a0d520495a8234a16
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-06-23 19:15:28 +0530

    Created basic for loop program in js

---
+++ JavaScript/Loops/ForLoop.js
@@ -0,0 +1,6 @@
+// Basic for loop structure
+//  initialization   condition    increment
+for(   let i=1;         i<=10;      i++){
+    // Block of Code
+    console.log("I am a loop and I am printing ",i)
+}
