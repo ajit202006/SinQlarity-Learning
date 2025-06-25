8fd16e484ed0edda42b67d7a33dce3eafcdb9f72
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-06-25 19:50:18 +0530

    Created code for unary operators

---
+++ JavaScript/Operators/UnaryOperators.js
@@ -0,0 +1,9 @@
+// Unary operator -> Work on single operand
+let x=5;
+
+x++;// post increment -- first uses x then add one 
+++x;// pre increment -- first add one in x then uses x
+x--;// post decrement -- first uses x then subtracts one from x
+--x;// pre decrement -- first subtracts one from x then uses it 
+
+console.log(x);
