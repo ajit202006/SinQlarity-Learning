fbaa745b6936a875e409c0b28aff9643c84f9411
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-06-25 19:54:28 +0530

    Created code for ternary operator

---
+++ JavaScript/Operators/TernaryOperator.js
@@ -0,0 +1,5 @@
+//Ternary operator -> Operates on three operands
+let x=45,y=5;
+// Using ternary operator to find greater of two number 
+// condition ? if true this executes       :   otherwise this ;
+x>y          ? console.log("Greater is",x) : console.log("Greater is",y);
