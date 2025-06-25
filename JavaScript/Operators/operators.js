c301718e52521bdcb2a4e2c3c441ca361ec38364
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-06-25 19:46:07 +0530

    Created code for different types of operators

---
+++ JavaScript/Operators/operators.js
@@ -0,0 +1,35 @@
+// Arithmetic operators return numerical values
+let x = 6 + 7; //  Addition operator 
+let y = 6 - 7; //  Subtraction operator 
+let z = 6 * 7; //  Multiplication operator 
+let w = 6 / 7; //  Division  operator 
+let v = 6 % 7; //  Modulus  operator 
+let u = 6**2;  //  power operator raises 6 to the power of 2
+
+// Assignment operator '='
+let a=9; // assignment operator used to assign value 9 in a 
+
+// Relational operators -> compare and return boolean values
+x<5; // less than
+x>5; // greater than
+x==5; // equal to
+x>=5; // greater than and equal to 
+x<=5; // less than and equal to 
+
+
+// Logical operators -> performed on booleans return booleans
+
+true && false; // Logical AND operator
+true || false; // Logical OR operator
+! true ; // Logical NOT
+
+// Bitwise operators -> perform operations between bits of two nuumbers
+
+5 & 6; // Bitwise AND
+5 | 6; // Bitwise OR
+5 << 6; // Bitwise left shift
+5 >> 6; // Bitwise right shift
+5^6; // Bitwise XOR 
+
+
+
