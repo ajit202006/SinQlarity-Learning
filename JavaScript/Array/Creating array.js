f02bfaa003ad25c2a7ad921c4edc2087aa0393f1
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-06-26 10:31:01 +0530

    Created code of array creation and population

---
+++ JavaScript/Array/Creating array.js
@@ -0,0 +1,19 @@
+// methods of delaring an array
+// Method 1: 
+let arr=[34,4,5,56,654,45,32,23,47];
+// Method 2: 
+let arr1=Array(34,4,5,56,654,45,32,23,47);
+// Method 3: 
+let arr2=new Array(34,4,5,56,654,45,32,23,47);
+
+
+// Populating array -> Creating an array by assining value to each element
+// First declare an array by one of above methods
+let arrEx=[];
+// then assign value one by one to each element
+arrEx[0]=4;
+arrEx[1]=5;
+arrEx[2]=6;
+arrEx[3]=7;
+
+console.log(arrEx);
