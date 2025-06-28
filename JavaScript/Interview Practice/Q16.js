66cf6a550a7ee883d49f1128463c542e5a074f19
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-06-28 21:57:11 +0530

    Solved question 16 of interview practice

---
+++ JavaScript/Interview Practice/Q16.js
@@ -0,0 +1,6 @@
+// Create function argumentsLength that returns number of arguments passed to it
+function argumentsLength(...args){
+    return args.length;
+}
+
+console.log(argumentsLength(1,2,23,3,123,123));
