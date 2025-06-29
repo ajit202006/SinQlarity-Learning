e133c5ad0b084d7354f0a1a050c6e68a763cbaa4
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-06-29 11:24:04 +0530

    Practiced set objects in ES6

---
+++ ES6 ES2020/ES6/15. Set Objects.js
@@ -0,0 +1,10 @@
+// Creating a set
+let newSet = new Set();
+newSet.add(1);
+newSet.add(4);
+newSet.add(3);
+newSet.add(2);
+newSet.add(4);// no duplicates allowed
+newSet.add(5);
+
+console.log(newSet);
