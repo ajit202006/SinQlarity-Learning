1a2a5ef48d3b8560fc7a77ba679c8ad5e99b392a
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-06-29 12:19:05 +0530

    Practiced global methods in ES6

---
+++ ES6 ES2020/ES6/23. Global Methods.js
@@ -0,0 +1,12 @@
+// isFinite
+console.log("Finite");
+console.log(isFinite(Infinity));
+console.log(isFinite(5));
+console.log(isFinite(5/0));
+
+// is NaN
+console.log("NAN");
+console.log(isNaN(6));
+console.log(isNaN("5a5"));
+console.log(isNaN(undefined));
+console.log(isNaN(Infinity));
