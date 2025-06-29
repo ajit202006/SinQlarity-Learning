129cf73f27cb4ba9f4fb489430a52ea9791f9d53
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-06-29 11:47:46 +0530

    Practiced string methods in ES6

---
+++ ES6 ES2020/ES6/19. String Methods.js
@@ -0,0 +1,7 @@
+let str="Hi there I am a string do you know me.";
+// .includes()
+console.log(str.includes("I"));
+// .startsWith()
+console.log(str.startsWith("Hi"));
+// .endsWith()
+console.log(str.endsWith("me."));
