0cb11d66918ca91deb241808bf1e7a527d91e25e
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-06-29 12:14:40 +0530

    Practiced Number properties methods in ES6

---
+++ ES6 ES2020/ES6/22. Number Methods.js
@@ -0,0 +1,18 @@
+// Number.EPSILON
+let eps=Number.EPSILON;
+console.log(eps);
+
+// Number.MIN_SAFE_INTEGER
+let minS=Number.MIN_SAFE_INTEGER;
+console.log(minS);
+
+// Number.MAX_SAFE_INTEGER
+let maxS=Number.MAX_SAFE_INTEGER;
+console.log(maxS);
+
+// Number.isInteger()
+console.log(Number.isInteger(4));
+console.log(Number.isInteger(4.666));
+// Number.isFloat()
+console.log(Number.isSafeInteger(maxS));
+console.log(Number.isSafeInteger(maxS+1));
