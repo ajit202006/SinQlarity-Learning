ac571ede0fcf26537dd078378c16c9c8cb6e85fa
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-06-29 12:08:10 +0530

    Practiced Math methods in ES6

---
+++ ES6 ES2020/ES6/21. Math Object.js
@@ -0,0 +1,15 @@
+// Math.trunc()
+console.log(Math.trunc(5.67674));
+
+// Math.sign()
+console.log(Math.sign(5));
+console.log(Math.sign(-5));
+console.log(Math.sign(0));
+
+// Math.cbrt() -> cuberoot
+console.log(Math.cbrt(120))
+console.log(Math.cbrt(125))
+
+// Math.log2()
+console.log(Math.log2(4));
+console.log(Math.log10(1000));
