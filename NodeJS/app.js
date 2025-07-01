61d6e87a8d1f63c64b62e9e883d85de7fadd198b
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-07-01 17:31:03 +0530

    Added code to send response to user

--- NodeJS/app.js
+++ NodeJS/app.js
@@ -1,8 +1,12 @@
 const express=require('express');
 const app=express();
 
+// app.get('/',(req,res)=>{
+//     console.log(req);
+// });
+
 app.get('/',(req,res)=>{
-    console.log(req);
+    res.send("Sent you a response");
 });
 
 app.listen(3000);
