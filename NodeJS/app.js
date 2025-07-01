4b5b0f6ae07aa4ca9daae27838cbe9143ba90d1b
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-07-01 19:39:38 +0530

    Added functionalities to use the router to route to controller

--- NodeJS/app.js
+++ NodeJS/app.js
@@ -1,12 +1,17 @@
 const express=require('express');
+
+const calculatorRouter=require('./Router/Calculator');
+
 const app=express();
 
+app.use(express.json())
 // app.get('/',(req,res)=>{
 //     console.log(req);
 // });
 
-app.get('/',(req,res)=>{
-    res.send("Sent you a response");
-});
+// app.get('/',(req,res)=>{
+//     res.send("Sent you a response");
+// });
 
+app.use('/calculator',calculatorRouter);
 app.listen(3000);
