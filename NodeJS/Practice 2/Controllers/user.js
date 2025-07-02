cfd6ae2339e4216ccb258a6668b93769fbaa5b88
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-07-02 18:54:28 +0530

    Added data retrieval functionalities

--- NodeJS/Practice 2/Controllers/user.js
+++ NodeJS/Practice 2/Controllers/user.js
@@ -1,17 +1,33 @@
 const userModel=require('../Models/user');
 
-module.exports.register=(req,res)=>{
-    userModel.insert(req.body);
-    res.send(req.body);
-}
-module.exports.get=(req,res)=>{
-    res.send(req.body);
+
+//register
+module.exports.register= async (req,res)=>{
+    let response = await userModel.insert(req.body);
+    if (response){
+        res.send({status:"Success",id:response});
+    }else{
+        res.send({status:"Failed",id:0,message:"User registreation failed."});
+    }
 }
 
+//get
+module.exports.get=async (req,res)=>{
+    let data =  await userModel.get();
+    if (data){
+        res.send({status:"Success",data:data});
+    }else{
+        res.send({status:"Failure",message:"Cannot fetch data"});
+    }
+}
+
+
+//update
 module.exports.update=(req,res)=>{
     res.send("Updating...");
 }
 
+//delete
 module.exports.delete=(req,res)=>{
     res.send("Deleting from the database.....");
 }
