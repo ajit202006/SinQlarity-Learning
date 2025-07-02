7e5cf994afb073de3eb908149ab503df449c9a2f
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-07-02 10:05:37 +0530

    Added code for more user actions

--- NodeJS/Practice 2/Routers/user.js
+++ NodeJS/Practice 2/Routers/user.js
@@ -6,7 +6,14 @@ const router=express.Router();
 
 // parses '/users/register'  requests
 router.use('/register',userController.register);
-router.use('/delete/update',userController.update);
+
+// parses '/users/get'  requests
+router.use('/get',userController.get);
+
+// parses '/users/update'  requests
+router.use('/update',userController.update);
+
+// parses '/users/delete'  requests
 router.use('/delete',userController.delete);
 
 
