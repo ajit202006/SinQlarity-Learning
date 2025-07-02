dcd031324b33d00d9cc6dfd6c71c9e4b8a3cfc74
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-07-02 10:57:42 +0530

    Added get function to the controller

--- NodeJS/Practice 2/Controllers/user.js
+++ NodeJS/Practice 2/Controllers/user.js
@@ -4,6 +4,9 @@ module.exports.register=(req,res)=>{
     userModel.insert(req.body);
     res.send(req.body);
 }
+module.exports.get=(req,res)=>{
+    res.send(req.body);
+}
 
 module.exports.update=(req,res)=>{
     res.send("Updating...");
