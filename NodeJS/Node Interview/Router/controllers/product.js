411e4defb610ed859b954c887de094b305b4427c
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-07-05 19:00:28 +0530

    Created controller file to control /product query actions

---
+++ NodeJS/Node Interview/Router/controllers/product.js
@@ -0,0 +1,6 @@
+module.exports.product=(req,res)=>{
+    res.send("Product name is being fetched...");
+}
+module.exports.orderNumber=(req,res)=>{
+    res.send("Product order number is being fetched...");
+}
