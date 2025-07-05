93032bfaa6b5494ab98e542a9976d2638853badf
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-07-05 12:07:19 +0530

    Created code to diffrentiate array from map object

---
+++ NodeJS/Node Interview/Array vs Map.js
@@ -0,0 +1,15 @@
+// An array is a collection of items stored at contiguous memory locations
+// generlly used to store MULTIPLE items of SAME datatypes
+// index of items is automatically decided
+let arr=[4,5,6,3,2,4,6];
+
+
+// Map => an associative dta type used to store value in mapped manner ( key-value pairs)
+// Used to store records of real life entities
+// indices needs to be declared along with their values
+
+let map= {
+    "sname":"Student 1 ",
+    rollNo:234,
+    class:2
+}
