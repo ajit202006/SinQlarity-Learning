5b7f6ed57a3617914086f6f8cb5705f5e4887918
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-07-18 20:29:06 +0530

    Added code for accessing alarm in mobile devices

--- Projects/VisualClock/JavaScript/alarm.js
+++ Projects/VisualClock/JavaScript/alarm.js
@@ -3,128 +3,58 @@ let select_hour = document.getElementById('hour');
 let select_minute = document.getElementById('minute');
 let select_meridian = document.getElementById('meridian');
 let add_alarm_btn = document.getElementById('add-alarm');
-let remove_alarm_btn = document.getElementById('remove-alarm');
 let alarm_sound = document.getElementById('alarm-sound');
 let alarm_list = document.getElementById('alarm-list');
 let alarm_list_label = document.getElementById('alarm-label');
+let dropdown_btn4=document.getElementById('dropdown-btn4');
+let header4=document.getElementById('dropdown-list4');
 let alarm_array = [];
 let alarm_count = 0;
 let timeout_object = {};
-let snooze_flag = false;
 let stop_flag = false;
-let alarm_interval=null;
+let alarm_interval = null;
+
+
 // adding options in the select tag
 for (let i = 1; i < 13; i++) {
-    select_hour.innerHTML += `<option val=${i}>${i}</option>`;
+    select_hour.innerHTML += `<option val=${i}>${i >= 10 ? i : '0' + i}</option>`;
 }
 for (let i = 0; i < 60; i++) {
-    select_minute.innerHTML += `<option val=${i}>${i}</option>`;
+    select_minute.innerHTML += `<option val=${i}>${i >= 10 ? i : '0' + i}</option>`;
 }
 
-//// OLd code ////
-
-// // Function to set an alarm
-// let alarm_interval = null;
-// function setAlarm(offset = 0) { // offset decides if the alarm is snoozed of not
-//     // getting values inputted in alarm values
-//     let hours = select_hour.value;
-//     let minutes = +(select_minute.value) + offset;
-//     let meridian = select_meridian.value;
-//     // showing alarm time on screen
-//     alarm.innerHTML = `${hours < 10 ? '0' + hours : hours} : ${minutes < 10 ? '0' + minutes : minutes}<span>&nbsp;${meridian}</span>`;
-
-//     // converting to 24 hour system
-//     if (meridian == "AM" && hours == 12) {
-//         hours = 0;
-//     } else if (meridian == "PM" && hours != 12) {
-//         hours = 12 + (+hours);
-//     }
-
-//     // adding an interval to check for alarm time 
-//     alarm_interval = setInterval(compareTime, 2000, hours, minutes);
-// }
-
-
-// // comparing current time with alarrm time and executing alarm when time ends
-// function compareTime(hours, minutes) {
-//     let date = new Date();// creating a date object to get current time and date
-//     let curr_hour = date.getHours();// getting current hours
-//     let curr_minute = date.getMinutes();// getting current minutes
-//     if (curr_hour == hours && curr_minute == minutes) {
-//         alarm_sound.play();// playing sound when alarm goes off
-//         stop_alarm_btn.disabled = false;// enabling the stop button
-//         set_alarm_btn.disabled = false;// enabling the set/snooze button
-//     }
-// }
-// let snoozer = false;// value to allow snoozing the alarm
-// // settingg an alarm on click
-// set_alarm_btn.addEventListener('click', () => {
-//     // checking if alarm is snoozed or new 
-//     if (snoozer) {
-//         clearInterval(alarm_interval);
-//         setAlarm(5);
-//         set_alarm_btn.disabled = true;
-//     } else {
-//         setAlarm();
-//         set_alarm_btn.innerHTML = "Snooze";
-//         set_alarm_btn.disabled = true;
-//         snoozer = true;
-//         stop_alarm_btn.disabled = true;
-//     }
-// });
-
-// // stopping the alarm on click of stop button
-// stop_alarm_btn.addEventListener('click', () => {
-//     clearInterval(alarm_interval);
-//     set_alarm_btn.disabled = false;
-//     set_alarm_btn.innerHTML = "Set";
-//     snoozer = false;
-// });
 
 //// New Code ////
 
 // function to play ringtone when alarm goes off
 function callAlarm(alarm_id) {
-    add_alarm_btn.disabled=true;
+    let curr_alarm = document.getElementById(`li-${alarm_id}`);
+    // Ringing the alarm tune at every two second
     alarm_sound.play();
-    deleteAlarm(alarm_id);
-    remove_alarm_btn.innerHTML="Stop";
     alarm_interval = setInterval(() => {
         alarm_sound.play();
     }, 2000);
-    stop_flag=true;
+
+    // Changin the add button to stop button
+    add_alarm_btn.innerHTML = "Stop";
+    stop_flag = true;
+
+    // dislaying the alarm time
+    alarm.innerHTML = curr_alarm.innerText.slice(0, 8);
+    alarm.style.display = "flex";
+
+    // Stopping alarm ring after one minute
     setTimeout(() => {
+        alert("Alarm Ringing");
         clearInterval(alarm_interval);
-        stop_flag=false;
-        remove_alarm_btn.innerHTML="Remove";
-        add_alarm_btn.disabled=false;
+        stop_flag = false;
+        add_alarm_btn.innerHTML = "Add";
+        alarm.style.display = "none";
+        let curr_timeout = setTimeout(callAlarm, (24 * 60 * 60 - 60) * 1000, alarm_id);
+        timeout_object[alarm.innerHTML] = curr_timeout;
     }, 58000);
 }
 
-// function to delete alarm
-function deleteAlarm(alarm_id = alarm_list.value) {
-    // getting element to be removed into a variable by its id
-    let removed_alarm = document.getElementById(`${alarm_id}`);
-
-    // removing the alarm from the alarm array
-    alarm_array = alarm_array.filter(element => element != removed_alarm.innerHTML);
-    // Clearin the alarm timeout as it is no longer needed
-    clearTimeout(timeout_object[removed_alarm.innerHTML]);
-
-    // deleting the alarm
-    delete timeout_object[removed_alarm.innerHTML];
-
-    // removing the alarm from the html document
-    removed_alarm.remove();
-
-    alarm_count--;//decrementing number of alarm
-
-    if (alarm_count == 0) {
-        alarm_list.style.display = "none";// hiding select alarm button
-        alarm_list_label.style.display = "none";// hiding select alarm label
-        alarm.innerHTML="00 : 00 <span>AM</span>";
-    }
-}
 
 // function to calculate alarm time difference from current time
 function calculateTime(hours, minutes, meridian) {
@@ -168,25 +98,76 @@ function calculateTime(hours, minutes, meridian) {
     return [hours, minutes, curr_second ? 60 - curr_second : 0];
 }
 
+// Function to remove an alarm
+function removeAlarm(alarm_id) {
+
+    // Accessing element from html document
+    let to_be_removed = document.getElementById(`li-${alarm_id}`);
+    let alarm_time = to_be_removed.innerText.slice(0, 8);
+
+    // removing the alarm from the alarm array
+    alarm_array = alarm_array.filter(element => element != alarm_time);
+
+    // Clearing the alarm timeout as it is no longer needed
+    clearTimeout(timeout_object[alarm_time]);
+    // deleting the alarm
+    delete timeout_object[alarm_time];
+
+    // removing the alarm from html document 
+    to_be_removed.remove();
+
+    alarm_count--;//decrementing number of alarm
+
+    if (alarm_count == 0) {
+        alarm_list.style.opacity = 0;// hiding select alarm button
+        alarm_list_label.style.opacity = 0;// hiding select alarm label
+    }
+}
+
 // Adding an alarm
 add_alarm_btn.addEventListener('click', () => {
     if (alarm_count == 19) {
         // alerting if number of alarms exceeded 20
         alert("Only 20 alarms are allowed at a time");
-    } else {
+    } else if (stop_flag) {
+        let curr_second = (new Date()).getSeconds();// getting second when stop buttn is clicked
+        // stopping the alarm
+        clearInterval(alarm_interval);
+        // Changing stop button to add button
+        stop_flag = false;
+        add_alarm_btn.innerHTML = "Add";
+        // hiding alarm dislay
+        alarm.style.display = "none";
+        // Getting the id of running alarm
+        let alarm_id = null;
+        for (let alarm_id = 0; alarm_id < 20; alarm_id++) {
+            if (document.getElementById(`li-${alarm_id}`).innerText.slice(0, 8) == alarm.innerHTML) {
+                break;
+            }
+        }
+        // setting same alarm for the next day
+        let curr_timeout = setTimeout(callAlarm, (24 * 60 * 60 - 60 + curr_second) * 1000, alarm_id);
+        timeout_object[alarm.innerHTML] = curr_timeout;
+    }
+    else {
         let hour = +select_hour.value;
         let minute = +select_minute.value;
         let meridian = select_meridian.value;
         let formatted_time = `${hour < 10 ? '0' + hour : hour}:${minute < 10 ? '0' + minute : minute} ${meridian}`;
 
         if (!alarm_array.includes(formatted_time)) {
-            alarm.innerHTML=`${hour < 10 ? '0' + hour : hour} : ${minute < 10 ? '0' + minute : minute} <span>${meridian}</span>`;
-            let option = document.createElement('option');
-            option.setAttribute('id', `${alarm_count}`);
-            option.setAttribute('value', `${alarm_count}`);
+            let li = document.createElement('li');// Creating a list item
+            let btn = document.createElement(`button`);// Creating a button
+            li.setAttribute('id', `li-${alarm_count}`);// giving an id to list item
+            btn.setAttribute('id', `${alarm_count}`);// giving id to button
 
-            option.innerHTML = formatted_time;
+            // adding removeAlarm function as onclick event for button
+            btn.setAttribute('onclick', `removeAlarm(${alarm_count})`);
+            li.innerHTML = formatted_time;
+            btn.innerHTML = "Delete";
 
+            // adding button to list item
+            li.appendChild(btn);
 
             let second = 0;
             [hour, minute, second] = calculateTime(hour, minute, meridian);
@@ -197,16 +178,22 @@ add_alarm_btn.addEventListener('click', () => {
             let curr_timeout = setTimeout(callAlarm, second * 1000, alarm_count);
 
             // adding alarm option to the html
-            alarm_list.appendChild(option);
+            alarm_list.appendChild(li);
 
             // making alarm list visible 
-            alarm_list.style.display = "inline";
-            alarm_list_label.style.display = "inline";
+            alarm_list_label.style.display = "block";
+            alarm_list_label.style.opacity = 1;
+            alarm_list.style.opacity = 1;
+            if (alarm_count >= 3) {
+                alarm_list.style.overflowY = "scroll";
+                alarm_list.style.scrollSnapType = "y mandatory";
+            }
 
             // adding alarm and its timeout to arrays
             alarm_array.push(formatted_time);
             timeout_object[formatted_time] = curr_timeout;
-
+            alarm_array.sort();
+            console.log(alarm_array);
             alarm_count++; // incrementing number of alarms
         } else {
             // sending message if user tried to add multiple alarms
@@ -215,20 +202,16 @@ add_alarm_btn.addEventListener('click', () => {
     }
 });
 
-// deleting an alarm
-remove_alarm_btn.addEventListener('click', () => {
-    if (stop_flag){
-        clearInterval(alarm_interval);
-        stop_flag=false;
-        remove_alarm_btn.innerHTML="Remove";
-        add_alarm_btn.disabled=false;
+let close_flag4=false;
+dropdown_btn4.onclick=()=>{
+    if (close_flag4){
+        header4.style.display='none'
+        dropdown_btn4.innerHTML='Menu';
+        close_flag4=false;
     }else{
-        if (!alarm_count) {
-            // alerting when trying to delete a not set alarm
-            alert("There are no alarms for today");
-        } else {
-            // otherwise deleting alarm
-            deleteAlarm();
-        }
+        
+        header4.style.display='flex';
+        dropdown_btn4.innerHTML='X';
+        close_flag4=true;
     }
-});
+}
