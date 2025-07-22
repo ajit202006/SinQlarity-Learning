626c0584db2e5566e92a41aa2090b41a4f848ea2
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-07-22 17:09:54 +0530

    Added Custom wrapper to the list item

--- React/routing-in-react/src/components/products/ProductItem.js
+++ React/routing-in-react/src/components/products/ProductItem.js
@@ -1,12 +1,15 @@
 import Styles from "./ProductItem.module.css";
+import CustomWrapper from "./CustomWrapper";
 function ProductItem(props) {
     return (
         <li className={Styles.li}>
-            <img src={props.item.image} alt="Not found"/>
-            <h1>{props.item.product_name}</h1>
-            <p>{props.item.description}</p>
-            <h4>{props.item.price}</h4>
-            <button>Purchase</button>
+            <CustomWrapper>
+                <img src={props.item.image} alt="Not found" />
+                <h1>{props.item.product_name}</h1>
+                <p>{props.item.description}</p>
+                <h4>{props.item.price}</h4>
+                <button>Purchase</button>
+            </CustomWrapper>
         </li>
     )
 }
