3efd0fd2a2528b982111c210c2b9880f005dcfaa
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-07-22 14:08:14 +0530

    Added the main menu to navigate to different routes

--- React/routing-in-react/src/App.js
+++ React/routing-in-react/src/App.js
@@ -2,9 +2,11 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import { Routes, Route } from 'react-router-dom';
+import MainMenu from './components/MainMenu';
 function App() {
   return (
     <div>
+      <MainMenu/>
       <Routes>
         <Route path='/' element={<Home />}></Route>
         <Route path='/about' element={<About />}></Route>
