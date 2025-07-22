d556d8684f81df84c04602c0228c67c80f80193c
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-07-22 16:39:00 +0530

    Added detailed description of products and changed product listing method

--- React/routing-in-react/src/pages/Product.js
+++ React/routing-in-react/src/pages/Product.js
@@ -1,19 +1,24 @@
+import ProductList from "../components/products/ProductList";
 function Product() {
     let products = [
         {
-            id:'p1',
-            product_name:"City Tour"
+            id: 'p1',
+            product_name: "City Tour",
+            image: 'https://cdn.pixabay.com/photo/2022/05/03/10/29/prague-7171444_1280.jpg',
+            description: "Its a city tour",
+            price: "20000"
         },
         {
-            id:'p2',
-            product_name:"Forest Tour"
+            id: 'p2',
+            product_name: "Forest Tour",
+            image: 'https://cdn.pixabay.com/photo/2022/07/09/18/14/forest-7311484_1280.jpg',
+            description: "Its a forest tour",
+            price: "50000"
         }
     ];
     return (
         <div>
-            <ul>
-                {products.map(item=><li>{"Tour Type : "+item.product_name}</li>)}
-            </ul>
+            <ProductList products={products} />
         </div>
     );
 }
