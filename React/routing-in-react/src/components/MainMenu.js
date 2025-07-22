1e54b1f9bd87f839d5d41c415e145d79a210f255
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-07-22 15:37:15 +0530

    Added route to product page in Main Menu

--- React/routing-in-react/src/components/MainMenu.js
+++ React/routing-in-react/src/components/MainMenu.js
@@ -8,6 +8,7 @@ function MainMenu(){
                     <li><Link to="/"><button>Home</button></Link></li>
                     <li><Link to="/about"><button>About</button></Link></li>
                     <li><Link to="/contact"><button>Contact</button></Link></li>
+                    <li><Link to="/product"><button>Product</button></Link></li>
                 </ul>
             </nav>
         </header>
