017f3bd3fb639a26ae8b8d51eccae2517d992886
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-07-22 19:58:57 +0530

    Added code to send data to the backend to store in the database

--- React/routing-in-react/src/pages/AddProduct.js
+++ React/routing-in-react/src/pages/AddProduct.js
@@ -1,5 +1,16 @@
 import AddProductForm from "../components/products/AddProductForm";
-function AddProduct(){
-    return <AddProductForm/>
+function AddProduct() {
+    function addProductHandler(newProduct) {
+        fetch("http://localhost:3002/product", {
+            method: "POST",
+            body: JSON.stringify(newProduct),
+            headers: {
+                'Content-Type': "application/json"
+            }
+        }).then(response=>response.json())
+        .then(data=>console.log(data))
+        .catch(err=>console.log(err));
+    }
+    return <AddProductForm addProductHandler={addProductHandler} />
 }
 export default AddProduct;
