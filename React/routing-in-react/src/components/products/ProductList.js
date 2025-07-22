bb0a83a81d253d4f49ad7767263d85d6fb8994c2
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-07-22 16:40:06 +0530

    Created file that returns product list with all items

---
+++ React/routing-in-react/src/components/products/ProductList.js
@@ -0,0 +1,11 @@
+import ProductItem from "./ProductItem";
+function ProductList(props) {
+    return (
+        <ul>
+            {props.products.map((item) => {
+                return <ProductItem key={item.id} id={item.id} item={item} />
+            })}
+        </ul>
+    )
+}
+export default ProductList;
