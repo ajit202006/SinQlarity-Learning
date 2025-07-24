280ffb5ed792bdd665af6aa41846900e0e63eebe
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-07-24 15:07:31 +0530

    Added add to favourite button and its functionality

--- React/routing-in-react/src/components/products/ProductItem.js
+++ React/routing-in-react/src/components/products/ProductItem.js
@@ -1,6 +1,22 @@
 import Styles from "./ProductItem.module.css";
 import CustomWrapper from "./CustomWrapper";
+import { useContext } from "react";
+import FavouriteContext from "../store/contextFavourite";
+
 function ProductItem(props) {
+
+    const favouriteContext=useContext(FavouriteContext);
+
+    const isFavourite= favouriteContext.isFavouriteItem(props.item._id);
+    const toggleFavourite=()=>{
+        if (isFavourite){
+            favouriteContext.removeFavouriteItem(props.item._id);
+            console.log(favouriteContext.favouriteItems);
+        }else{
+            favouriteContext.addFavouriteItem({...props.item});
+            console.log(favouriteContext.favouriteItems);
+        }
+    }
     return (
         <li className={Styles.li}>
             <CustomWrapper>
@@ -9,6 +25,7 @@ function ProductItem(props) {
                 <p>{props.item.description}</p>
                 <h4>{props.item.price}</h4>
                 <button>Purchase</button>
+                <button onClick={toggleFavourite}>Favourite</button>
             </CustomWrapper>
         </li>
     )
