c052d56198b89c5fadbb908bda74798477153f83
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-07-24 14:39:02 +0530

    Added wrapper context favourite to the whole app

--- React/routing-in-react/src/index.js
+++ React/routing-in-react/src/index.js
@@ -1,8 +1,13 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { ContextFavourite } from './components/store/contextFavourite';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter><App /></BrowserRouter>
+  <ContextFavourite>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ContextFavourite>
 );
