9aa3ba211d7b44521e83510bbf169f7902e23927
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-07-26 17:54:42 +0530

    Added function to find the winner at every stage

--- Projects/TicTacToe/tic-tac-toe/src/App.js
+++ Projects/TicTacToe/tic-tac-toe/src/App.js
@@ -1,3 +1,19 @@
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],//Winning horizontally
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],// Winning vertically
+    [0, 4, 8], [2, 4, 6],// Winning diagonally
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[b] === squares[c]) {
+      return [squares[a], a, b, c];
+    }
+  }
+  return null;
+}
+
 function Square({ value, onSquareClick, highlight }) {
   return <button className={`square ${highlight ? " highlight" : ""}`} onClick={onSquareClick}>{value}</button>
 }
