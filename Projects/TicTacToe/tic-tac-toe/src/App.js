936243fa39923577bbc0a65791fa55f70a330a0a
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-07-26 17:56:32 +0530

    Added game component to set different game states at runtime and also to render the board on the app

--- Projects/TicTacToe/tic-tac-toe/src/App.js
+++ Projects/TicTacToe/tic-tac-toe/src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react"; 
+
 function calculateWinner(squares) {
   const lines = [
     [0, 1, 2], [3, 4, 5], [6, 7, 8],//Winning horizontally
@@ -71,6 +73,71 @@ function Board({ xIsNext, squares, onPlay, currentMove, resetBoard }) {
         </div>
       </div>
       <div className="status"> <button onClick={resetBoard}>Reset</button></div>
+    </>
+  );
+}
+
+export default function Game() {
+  const [history, setHistory] = useState([Array(9).fill(null)])
+  const [currentMove, setCurrentMove] = useState(0);
+  const [isIncreasing, setIsIncreasing] = useState(false);
+  const currentSquares = history[currentMove];
+  const xIsNext = currentMove % 2 === 0;
+  const [row, setRow] = useState([]);
+  const [column, setColumn] = useState([]);
+  function handlePlay(nextSquares, i) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+    let tempRow = row.slice();
+    let tempColumn = column.slice();
+    tempRow.push(Math.floor(i / 3) + 1);
+    tempColumn.push(i % 3 + 1);
+    setRow(tempRow);
+    setColumn(tempColumn);
+  }
+
+  function jumpsTo(nextMove) {
+    setCurrentMove(nextMove);
+  }
+
+  function resetBoard() {
+    setCurrentMove(0);
+    setHistory([Array(9).fill(null)]);
+  }
+
+  const moves = !currentMove ? null : history.map((squares, move) => {
+    let description = `(${row[move-1]}, ${column[move-1]})`;
+    if (move === history.length - 1) {
+      description = `Current move ${description}`;
+    } else if (move > 0) {
+      description = `Move #${move} ${description}`;
+    } else {
+      return null;
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpsTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
+  return (
+    <>
+      <div className="game-board">
+        <Board
+          xIsNext={xIsNext}
+          squares={currentSquares}
+          onPlay={handlePlay}
+          currentMove={currentMove}
+          resetBoard={resetBoard}
+        />
+      </div>
+      <div className={currentMove ? "game-info" : "display-none"}>
+        <div className="status">History</div>
+        <ul>{isIncreasing ? moves : moves?.reverse()}</ul>
+        <button id="order" onClick={() => { setIsIncreasing(!isIncreasing) }}>{isIncreasing ? "Increasing" : "Decreasing"}</button>
+      </div>
     </>
   );
 }
