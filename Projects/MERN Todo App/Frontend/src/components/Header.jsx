20138ab2c04d32f06b703c6b6d710bb47a65be0f
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-07-31 21:02:12 +0530

    Created header component to display logo and tagline

---
+++ Projects/MERN Todo App/Frontend/src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+        <header className="bg-green-900 h-10 py-1 flex justify-center items-center text-white md:text-[16px] text-[14px]">
+            <img className="h-full" src="../../logo-bnw.png"/>
+            <h1 className=""><b className="md:text-2xl text-[20px] mx-2">Task Buddy :</b>Know your daily tasks</h1>
+        </header>
+    )
+}
+
+export default Header;
