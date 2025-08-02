70cf02342c8055bada0fc20beacddb184dc997c4
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-08-02 13:09:36 +0530

    Created a registration page

---
+++ Practice/MERN UserRegisterApplication/Frontend/src/pages/Register.jsx
@@ -0,0 +1,10 @@
+import Form from "../components/Form";
+const Register = () => {
+  return (
+    <div className="flex w-screen h-screen bg-emerald-100 justify-center items-center">
+      <Form/>
+    </div>
+  )
+}
+
+export default Register
