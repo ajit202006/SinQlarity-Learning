021212155057cb09f602ac60e1e6643ce21c779f
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-08-02 13:09:25 +0530

    Created a login page

---
+++ Practice/MERN UserRegisterApplication/Frontend/src/pages/Login.jsx
@@ -0,0 +1,10 @@
+import LoginForm from "../components/LoginForm";
+const Login = () => {
+  return (
+    <div className="flex w-screen h-screen bg-emerald-100 justify-center items-center">
+      <LoginForm/>
+    </div>
+  )
+}
+
+export default Login
