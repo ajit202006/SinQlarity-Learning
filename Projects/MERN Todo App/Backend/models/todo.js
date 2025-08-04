1244e341ec78e991203450e536f8c0fa9180fe53
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-08-04 11:35:38 +0530

    Updated databse that is being used

--- Projects/MERN Todo App/Backend/models/todo.js
+++ Projects/MERN Todo App/Backend/models/todo.js
@@ -1,33 +1,39 @@
-import Todo from "../database/todo.js"
+import User from "../database/users.js";
 
 let todoModel = {
-    create: async (data) => {
-        const result = await Todo.create(data);
+    createTask: async (userInfo) => {
+        const result = await User.findById(userInfo.id);
+        const newTodos = [...result.todos, { task: userInfo.task, isDone: userInfo.isDone }];
+        await result.updateOne({ todos: newTodos })
         return result._id;
     },
 
-    retrieve: async () => {
-        const data = await Todo.find();
-        return data;
+    retrieveTasks: async (userId) => {
+        const data = await User.findById(userId);;
+        return data.todos;
     },
 
     updateTask: async (id, task) => {
         try {
-            const current_todo = await Todo.findById(id);
+            const current_todo = await User.findById(id);
             await current_todo.updateOne({ isDone: !current_todo.isDone });
         } catch (error) {
             console.log(error);
         }
     },
-    markDone: async (id) => {
-        const current_todo = await Todo.findById(id);
-        await current_todo.updateOne({ isDone: !current_todo.isDone });
-        return current_todo;
+    markDone: async ({ id: userId, index: index }) => {
+        const userData = await User.findById(userId);
+        const newTodos= [...userData.todos];
+        newTodos[index].isDone=!(newTodos[index].isDone);
+        await userData.updateOne({ todos: newTodos });
     },
 
-    delete: async (id) => {
-        const current_todo = await Todo.deleteOne({ _id: id });
-        return current_todo;
+    deleteTask: async ({ id: userId, index: index }) => {
+        const userData = await User.findById(userId);
+        let newTodos = [...userData.todos];
+        newTodos.splice(index, 1);
+        await userData.updateOne({ todos: newTodos });
+        return true;
     }
 }
 
