be13c85e58054acb18b6f587f04272e073721686
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-08-04 11:30:53 +0530

    Updated method to fetch data of tasks

--- Projects/MERN Todo App/Backend/routers/todo.js
+++ Projects/MERN Todo App/Backend/routers/todo.js
@@ -4,7 +4,7 @@ import todoController from "../controllers/todo.js"
 const todoRouter=express.Router();
 
 // GET route to fetch data
-todoRouter.get("/",todoController.getTodo)
+todoRouter.post("/fetch",todoController.getTodo)
 // POST route to send data
 todoRouter.post("/",todoController.createTodo)
 // PUT route to update data
