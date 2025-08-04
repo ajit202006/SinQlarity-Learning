17a67582d107f9c7f1354f3efd4bd3259d006790
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-08-04 11:28:11 +0530

    Added user router

--- Projects/MERN Todo App/Backend/app.js
+++ Projects/MERN Todo App/Backend/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import todoRouter from "./routers/todo.js";
+import userRouter from "./routers/users.js";
 
 const app=express();
 
@@ -9,7 +10,8 @@ app.use(cors("http://localhost:5173"));
 
 app.use(express.json());
 
-app.use("/",todoRouter);
+app.use('/todos',todoRouter);
+app.use('/users',userRouter);
 
 await mongoose.connect(process.env.connection_string);
 app.listen(process.env.port,()=>{
