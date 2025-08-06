e82ab1079e53fc7b3b4506702ac2a0b5d97465b3
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-08-06 14:38:08 +0530

    Created router for /auth route

---
+++ Projects/QuizManager/src/routes/auth.ts
@@ -0,0 +1,50 @@
+import express from "express";
+import { registerUser, loginUser, isUserExist } from "../controllers/auth";
+import { body } from "express-validator";
+
+// routing /user queries
+const router = express.Router();
+
+
+// POST request for /auth
+router.post("/", [
+    body("name")
+        .trim()
+        .not()
+        .isEmpty()
+        .isLength({ min: 3 })
+        .withMessage("Please enter a valid name with at least 3 characters"),
+    body("email")
+        .trim()
+        .isEmail()
+        .normalizeEmail()
+        .custom((emailID:String) => {
+            return isUserExist(emailID)
+                .then((status) => {
+                    if (status) {
+                        return Promise.reject("User already exist");
+                    }
+                })
+                .catch((err) => {
+                    return Promise.reject(err);
+                })
+        })
+        ,
+    body("password")
+        .trim()
+        .isLength({ min: 8 })
+        .withMessage("Enter at least 8 characters in password"),
+    body("confirm_password")
+    .trim()
+    .custom((value,{req})=>{
+        if (value!=req.body.password){
+            return Promise.reject("Password Mismatch");
+        }
+        return true;
+    })
+], registerUser);
+
+// POST request for /auth/login
+router.post("/login",[body("email").isEmail().normalizeEmail()], loginUser);
+
+export default router;
