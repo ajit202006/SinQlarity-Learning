3bef7a5334c055a1998a2ca891c5df766cfad1e6
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-08-07 13:08:16 +0530

    Added validation to data in routes

--- Projects/QuizManager/src/routes/quiz.ts
+++ Projects/QuizManager/src/routes/quiz.ts
@@ -1,17 +1,65 @@
 import express from "express";
+import { body } from "express-validator";
+
 import { createQuiz, getQuiz, updateQuiz, deleteQuiz, publishQuiz } from "../controllers/quiz";
 import { isAuthenticated } from "../middlewares/isAuth";
 
 const router = express.Router();
 
 // Create quiz
-router.post('/', isAuthenticated, createQuiz);
+router.post('/', isAuthenticated, [
+    body("name")
+        .trim()
+        .not()
+        .isEmpty()
+        .isLength({ min: 4 })
+        .withMessage("Name should contain more than 3 characters."),
+    body("questions_list")
+        .custom((questions_list) => {
+            if (!questions_list.length) {
+                return Promise.reject("Questions list is empty.");
+            }
+            return true;
+        }),
+
+    body("answers")
+        .custom((value, { req }) => {
+            console.log("answers", Object.keys(value).length);
+            console.log("Questions ", req.body.questions_list.length);
+            if (Object.keys(value).length !== req.body.questions_list.length) {
+                return Promise.reject("'Answers' does not match 'Questions List'")
+            }
+            return true;
+        })
+], createQuiz);
 
 // get quiz
 router.get('/:quizId', isAuthenticated, getQuiz);
 
 //update quiz
-router.put('/', isAuthenticated, updateQuiz);
+router.put('/', isAuthenticated, [
+    body("name")
+        .trim()
+        .not()
+        .isEmpty()
+        .isLength({ min: 4 })
+        .withMessage("Name should contain more than 3 characters."),
+    body("questions_list")
+        .custom((questions_list) => {
+            if (!questions_list.length) {
+                return Promise.reject("Questions list is empty.");
+            }
+            return true;
+        }),
+
+    body("answers")
+        .custom((value, { req }) => {
+            if (Object.keys(value).length !== req.body.questions_list.length) {
+                return Promise.reject("'Answers' does not match 'Questions List'")
+            }
+            return true;
+        })
+], updateQuiz);
 
 // delete quiz
 router.delete('/:quizId', isAuthenticated, deleteQuiz);
