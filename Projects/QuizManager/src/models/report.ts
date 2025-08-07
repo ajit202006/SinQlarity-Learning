15ab0abec61d24acc221cd70568b814dc51b0a66
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-08-07 16:38:46 +0530

    Created schema for report collection

---
+++ Projects/QuizManager/src/models/report.ts
@@ -0,0 +1,27 @@
+import mongoose, { Schema } from "mongoose";
+
+const reportSchema = new Schema(
+    {
+        userId: {
+            type: mongoose.Types.ObjectId,
+            required: true
+        },
+        quizId: {
+            type: mongoose.Types.ObjectId,
+            required: true
+        },
+        score: {
+            type: Number,
+            required: true
+        },
+        total: {
+            type: Number,
+            required: true
+        }
+    },
+    { timestamps: true }
+);
+
+const Report = mongoose.model("Report",reportSchema);
+
+export default Report;
