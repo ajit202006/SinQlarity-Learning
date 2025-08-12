84784d94ed6f1f6d310f15750fb468593d410f53
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-08-12 16:56:38 +0530

    Added route to update password

--- Projects/QuizManager/src/routes/user.ts
+++ Projects/QuizManager/src/routes/user.ts
@@ -1,5 +1,6 @@
 import express from "express";
-import { getUser, updateUser } from "../controllers/user";
+import { body } from "express-validator";
+import { getUser, updateUser, updatePassword } from "../controllers/user";
 import { isAuthenticated } from "../middlewares/isAuth";
 
 // routing /user queries
@@ -9,7 +10,33 @@ const router = express.Router();
 router.get("/:userId", isAuthenticated, getUser);
 
 //PUT /user
-router.put("/",isAuthenticated, updateUser);
+router.put("/", isAuthenticated, updateUser);
+
+//PUT /user/update_password
+router.put("/update_password", isAuthenticated, [
+    body("current_password")
+        .trim()
+        .isLength({ min: 8 })
+        .withMessage("Enter at least 8 characters in password"),
+    body("new_password")
+        .trim()
+        .isLength({ min: 8 })
+        .withMessage("Enter at least 8 characters in password")
+        .custom((value, { req }) => {
+            if (value == req.body.current_password) {
+                return Promise.reject("new_password cannot be same as current_password");
+            }
+            return true;
+        }),
+    body("confirm_password")
+        .trim()
+        .custom((value, { req }) => {
+            if (value != req.body.new_password) {
+                return Promise.reject("confirm_password should be same as new_password");
+            }
+            return true;
+        })
+], updatePassword)
 
 
 export default router;
